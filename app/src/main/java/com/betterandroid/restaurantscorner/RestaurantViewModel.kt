c6f8697b7e9c9867cfbbd98dc2ac2fccb01d8770
Riderpool1 <akshayashokan1054.com>
2022-02-09 11:27:18 +0530

    null checks & split function responsibility

---
+++ app/src/main/java/com/betterandroid/restaurantscorner/RestaurantViewModel.kt
@@ -0,0 +1,19 @@
+package com.betterandroid.restaurantscorner
+
+class RestaurantViewModel {
+    fun getDisplayRestaurants(restaurants:List<Restaurant>):List<RestaurantDisplayItem>{
+        return restaurants.map { restaurant ->
+            return@map RestaurantDisplayItem(
+                id = restaurant.id,
+                displayName = "Restaurant ${restaurant.name}",
+                displayDistance = "at ${restaurant.distance} KM distance",
+                imageUrl = restaurant.imageUrl,
+                type = when (restaurant.type) {
+                    "EAT_IN" -> RestaurantType.EAT_IN
+                    "TAKE_AWAY" -> RestaurantType.TAKE_AWAY
+                    else -> RestaurantType.DRIVE_THROUGH
+                }
+            )
+        }
+    }
+}
